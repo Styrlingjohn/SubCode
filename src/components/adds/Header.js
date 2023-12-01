58ba63e70332162db08f9e060367bca0156d2126
Styrlingjohn <styrling_morris@yahoo.com>
2023-12-01 09:46:02 -0700

    added logo and title

--- src/components/adds/Header.js
+++ src/components/adds/Header.js
@@ -2,16 +2,15 @@ import React from "react";
 
 
 function Header() {
-    
 
 
-return(
-    <div className="header">
-        Hello
 
-        
-    </div>
-)
+    return (
+        <div className="header">
+            <img alt="Logo" className="logo" src="Logos/SubCode-logos_white.png" />
+            <h1 className="H-T"> S u b C o d e </h1>
+        </div>
+    )
 };
 
 
