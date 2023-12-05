18440848b5645d12184b957aec24220e01f2db2b
Styrlingjohn <styrling_morris@yahoo.com>
2023-12-05 11:51:16 -0700

    added static form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,8 +5,41 @@ import React from 'react';
 function Contact() {
 
     return (
-        <div className='C-B'>
-            COntact me here
+        <div className='container mt-5'>
+            <h1> Contact me! </h1>
+
+
+            <form className='row g-3'>
+                <div className='col-md-6'>
+                    <label for='firstName' className='form-label'> First Name</label>
+                    <input type='text' className='form-control' id="firstName"
+                        placeholder='First Name' required></input>
+                </div>
+                <div className='col-md-6'>
+                    <label for='lastName' className='form-label'> Last Name</label>
+                    <input type='text' className='form-control' id="lastName"
+                        placeholder='Last Name' required></input>
+                </div>
+                <div className='col-md-8'>
+                    <label for="emailInfo" className='form-label'> E-Mail </label>
+                    <input type='email' className='form-control' id='emailInfo'
+                        placeholder='JohnDoe@email.com' required></input>
+                </div>
+                <div className='col-md-4'>
+                    <label for='phoneNumber' className='form-label'> Phone Number </label>
+                    <input type='text' className='form-control' id='phoneNumber'
+                    required placeholder='123-456-7899'></input>
+                </div>
+
+                <div className='col-md-12'>
+                    <label for="comments" className='form-label'> </label>
+                    <textarea className='form-control' id='comments' rows='7' required></textarea>
+                </div>
+
+                <div className='col-md-12'>
+                    <button type='submit' className='btn btn-primary'> Submit </button>
+                </div>
+            </form>
         </div>
     );
 }
