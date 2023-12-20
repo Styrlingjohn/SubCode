74979ffe68b955e5df66d8f6f67ed72363f571db
Styrlingjohn <styrling_morris@yahoo.com>
2023-12-19 18:01:03 -0700

    added other field names

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -12,12 +12,12 @@ function Contact() {
             <form className='row g-3' action="https://formsubmit.co/b9095cf0d6d8d869bb3c16dd3627fdfa" method="POST">
                 <div className='col-md-6'>
                     <label for='firstName' className='form-label'> First Name</label>
-                    <input type='text' className='form-control' id="firstName"
+                    <input type='text' className='form-control' name='name' id="firstName"
                         placeholder='First Name' required></input>
                 </div>
                 <div className='col-md-6'>
                     <label for='lastName' className='form-label'> Last Name</label>
-                    <input type='text' className='form-control' id="lastName"
+                    <input type='text' className='form-control' name='Last Name' id="lastName"
                         placeholder='Last Name' required></input>
                 </div>
                 <div className='col-md-8'>
@@ -26,14 +26,14 @@ function Contact() {
                         placeholder='JohnDoe@email.com' required></input>
                 </div>
                 <div className='col-md-4'>
-                    <label for='phoneNumber' name= 'Phone Number' className='form-label'> Phone Number </label>
-                    <input type='text' className='form-control' id='phoneNumber'
+                    <label for='phoneNumber' className='form-label'> Phone Number </label>
+                    <input type='text' className='form-control' name='Phone Number' id='phoneNumber'
                     required placeholder='123-456-7899'></input>
                 </div>
 
                 <div className='col-md-12'>
                     <label for="comments" className='form-label'> </label>
-                    <textarea className='form-control' id='comments' rows='7' required></textarea>
+                    <textarea className='form-control' id='comments' name='comments' rows='7' required></textarea>
                 </div>
 
                 <div className='col-md-12'>
